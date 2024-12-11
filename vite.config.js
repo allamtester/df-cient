c23daec3dcddf9dda8c22669af708100b894e921
allamtester <allamtester@gmail.com>
2024-12-11 23:04:34 +0530

    Update vite.config.js remove api server again in vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,6 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://df-server.onrender.com', // Replace with your API's URL
-        changeOrigin: true,
-        secure: false
-      },
-    },
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
