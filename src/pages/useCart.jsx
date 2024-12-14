6f19c1dbe9f3052fb96a6895e8a1be952c672b86
allamtester <allamtester@gmail.com>
2024-12-14 04:13:41 +0000

    useCart localStorage with object

--- src/pages/useCart.jsx
+++ src/pages/useCart.jsx
@@ -2,22 +2,29 @@ import { useState } from "react";
 import { getCart, saveCart } from "../utils/localStorage";
 
 const useCart = () => {
-    const [cart, setCart] = useState(getCart());
+  const [cart, setCart] = useState(getCart());
 
-    const addToCart = (item) => {
-        const duplicate = [...cart, item];
-        const updatedCart = [...new Set(duplicate)];
-        setCart(updatedCart);
-        saveCart(updatedCart);
-    };
+  const addToCart = (productId) => {
+    const objData = { productId, quantity: 1 };
+    let updatedCart = cart.map((product) =>
+      product.productId == objData.productId
+        ? { productId: objData.productId, quantity: product.quantity + objData.quantity }
+        : product
+    );
+    if (!updatedCart.some(item => item.productId === objData.productId)) {
+      updatedCart.push(objData);
+    }
+    saveCart(updatedCart);
+    setCart(updatedCart);
+  };
 
-    const removeFromCart = (id) => {
-        const updatedCart = cart.filter((item) => item.id !== id);
-        setCart(updatedCart);
-        saveCart(updatedCart);
-    };
+  const removeFromCart = (id) => {
+    const updatedCart = cart.filter((product) => product.productId !== id);
+    saveCart(updatedCart);
+    setCart(updatedCart);
+  };
 
-    return { cart, addToCart, removeFromCart };
+  return { cart, addToCart, removeFromCart };
 };
 
 export default useCart;
