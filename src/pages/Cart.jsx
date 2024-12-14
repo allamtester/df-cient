eb8adff8b845b10bd1e04cb968867eda9b42546a
allamtester <allamtester@gmail.com>
2024-12-14 04:59:45 +0000

    remove matchProduct

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -61,7 +61,7 @@ function Cart() {
                             </TableRow>
                         </TableHeader>
                         <TableBody>
-                            {matchedProducts.map((element) => {
+                            {products.map((element) => {
                                 return (
                                     <TableRow key={element._id}>
                                         <TableCell><img src={image9} className="bg-white h-36 w-40  object-cover" alt="" /></TableCell>
